7fbe6d4eed8649e0a1bbf4924a1a98e6bd9ab2c3
kenboydz <kenboydz@gmail.com>
2019-04-04 15:01:45 +0800

    去掉了 books.db； 增加了 tools.py 文件；

---
+++ src/tools.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+'''my_home_server 中用到的工具集合
+'''
+import os
+import re
+import pickle
+from xml.dom.minidom import parse
+import xml.dom.minidom
+
+
+class BookLoader:
+    '''读取 book 用 class
+    '''
+
+    def __init__(self, ):
+        pass
+
+    def load_book(self, book_name)->dict:
+        '''将 book_name 读到内存中
+        '''
+        pass
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
