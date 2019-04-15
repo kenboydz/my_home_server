3f83e4c25ec233416e91f5e4f54856f00daaa474
kenboydz <kenboydz@gmail.com>
2019-04-15 14:30:03 +0800

    使用 bookstrap-vue 插件，实现了 bookstrap 和 vue 的共存；

---
+++ vue/my_home_server/src/components/BaseButton.vue
@@ -0,0 +1,19 @@
+<template>
+  <button type="button" class="btn btn-dark">{{ text }}</button>
+</template>
+
+<script>
+export default {
+  name: 'BaseButton',
+  props: {
+    text: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
--- vue/my_home_server/src/main.js
+++ vue/my_home_server/src/main.js
@@ -1,8 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import store from './store'
 
 Vue.config.productionTip = false
 
+
+import BootstrapVue from 'bootstrap-vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue)
+
+
 new Vue({
-  render: h => h(App),
+  router,
+  store,
+  render: h => h(App)
 }).$mount('#app')
