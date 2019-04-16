71109ae9cf95060d5663d2183a245a179c23a7ca
kenboydz <kenboydz@gmail.com>
2019-04-16 13:48:04 +0800

    将 bookpage.vue 的 paginationbar 放到父级组件中控制；

--- vue/my_home_server/src/components/BookMain.vue
+++ vue/my_home_server/src/components/BookMain.vue
@@ -1,36 +1,20 @@
 <template>
 
-  <b-container class="text-light bg-dark">
-    <b-row align-v="start">
-      <b-col>
-        <pagination-bar />
-      </b-col>
-    </b-row>
-    <b-row>
-      <b-col>
-        <p v-for="i in 50" :key="i">
-          {{i}}
-        </p>
-      </b-col>
-    </b-row>
-    <b-row align-v="end">
-      <b-col>
-        <pagination-bar />
-      </b-col>
-    </b-row>
-  </b-container>
+  <b-row>
+    <b-col>
+      <p v-for="i in 50" :key="i">
+        {{i}}
+      </p>
+    </b-col>
+  </b-row>
 
 </template>
 
 
 <script>
-import PaginationBar from '../components/PaginationBar.vue'
 
 export default {
-  name: 'BookMain',
-  components: {
-    PaginationBar
-  }
+  name: 'BookMain'
 }
 </script>
 
--- vue/my_home_server/src/views/BookPage.vue
+++ vue/my_home_server/src/views/BookPage.vue
@@ -7,12 +7,32 @@
     </b-col></b-row>
 
     <b-row>
+      <!-- 章节导航 -->
       <b-col>
         <book-navbar />
       </b-col>
+      <!-- 分页和内容显示 -->
       <b-col>
-        <book-main />
+        <!-- 顶部分页 -->
+        <b-row align-v="start">
+          <b-col>
+            <pagination-bar />
+          </b-col>
+        </b-row>
+        <!-- 内容显示 -->
+        <b-row>
+          <b-col>
+            <book-main />
+          </b-col>
+        </b-row>
+        <!-- 底部分页 -->
+        <b-row align-v="end">
+          <b-col>
+            <pagination-bar />
+          </b-col>
+        </b-row>
       </b-col>
+
     </b-row>
 
   </b-container>
@@ -23,63 +43,20 @@
 import TheHeader from '../components/TheHeader.vue'
 import BookNavbar from '../components/BookNavbar.vue'
 import BookMain from '../components/BookMain.vue'
+import PaginationBar from '../components/PaginationBar.vue'
 
 export default {
   name: 'BookPage',
   components: {
     TheHeader,
     BookNavbar,
-    BookMain
+    BookMain,
+    PaginationBar
   }
 }
 </script>
 
 
 <style scoped>
-.el-container {
-  background-color: rgb(47, 49, 53);
-}
-
-.el-aside {
-  height: 1000px;
-  text-align: left;
-}
-
-.el-main {
-  height: 1000px;
-}
-  /* .el-header, .el-footer {
-    background-color: #B3C0D1;
-    color: #333;
-    text-align: center;
-    line-height: 60px;
-  } */
-  
-  /* .el-aside {
-    background-color: #D3DCE6;
-    color: #333;
-    text-align: center;
-    line-height: 200px;
-  } */
-  
-  /* .el-main {
-    background-color: #E9EEF3;
-    color: #333;
-    text-align: center;
-    line-height: 160px;
-  } */
-  
-  /* body > .el-container {
-    margin-bottom: 40px;
-  } */
-  
-  /* .el-container:nth-child(5) .el-aside,
-  .el-container:nth-child(6) .el-aside {
-    line-height: 260px;
-  } */
-  
-  /* .el-container:nth-child(7) .el-aside {
-    line-height: 320px;
-  } */
 </style>
 
