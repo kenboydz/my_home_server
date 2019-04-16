8e6e4dd625d1e633d4991914b076fe27b4c57456
kenboydz <kenboydz@gmail.com>
2019-04-16 14:59:34 +0800

    将 book 的组件结构做了调整；

--- vue/my_home_server/src/components/bookpage/BookDisplay.vue
+++ vue/my_home_server/src/components/bookpage/BookDisplay.vue
@@ -7,7 +7,24 @@
     </b-col>
     <!-- chapter 显示 -->
     <b-col>
-      <book-display-chapter :chapter-content="chapterContent" />
+      <!-- 顶部分页 -->
+      <b-row align-v="start">
+        <b-col>
+          <pagination-bar />
+        </b-col>
+      </b-row>
+      <!-- chapter显示 -->
+      <b-row>
+        <b-col>
+          <book-display-chapter :chapter-content="chapterContent" />
+        </b-col>
+      </b-row>
+      <!-- 底部分页 -->
+      <b-row align-v="end">
+        <b-col>
+          <pagination-bar />
+        </b-col>
+      </b-row>
     </b-col>
   </b-row>
 
@@ -17,12 +34,14 @@
 <script>
 import BookDisplayChapter from './BookDisplayChapter.vue'
 import BookDisplayNavbar from './BookDisplayNavbar.vue'
+import PaginationBar from '../common/PaginationBar.vue'
 
 export default {
   name: 'BookDisplay',
   components: {
     BookDisplayChapter,
-    BookDisplayNavbar
+    BookDisplayNavbar,
+    PaginationBar
   },
   data: function () {
     return {
--- vue/my_home_server/src/components/bookpage/BookDisplayChapter.vue
+++ vue/my_home_server/src/components/bookpage/BookDisplayChapter.vue
@@ -1,44 +1,17 @@
 <template>
 
-  <b-container>
-    <!-- 顶部分页 -->
-    <b-row align-v="start">
-      <b-col>
-        <pagination-bar />
-      </b-col>
-    </b-row>
-    <!-- chapter显示 -->
-    <b-row>
-      <b-col>
-        {{ chapterContent }}
-      </b-col>
-    </b-row>
-    <!-- 底部分页 -->
-    <b-row align-v="end">
-      <b-col>
-        <pagination-bar />
-      </b-col>
-    </b-row>
-  </b-container>
+  <div>
+    {{ chapterContent }}
+  </div>
 
 </template>
 
 
 <script>
-import PaginationBar from '../common/PaginationBar.vue'
-
 export default {
   name: 'BookMain',
-  components: {
-    PaginationBar
-  },
   props: {
-    chapterContent: String,
-    charactersPerPage: {
-      type: Number,
-      default: 10
-    },
-    currentPageIndex: Number
+    chapterContent: String
   },
 
 }
