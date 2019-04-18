a9cc8bfd48b0a28bb9abddfb5daa4a5f97dd8ff8
kenboydz <kenboydz@gmail.com>
2019-04-18 16:40:46 +0800

    增加了 partIndex 的双向绑定；

--- vue/my_home_server/src/components/bookpage/BookDisplayNavbar.vue
+++ vue/my_home_server/src/components/bookpage/BookDisplayNavbar.vue
@@ -42,11 +42,11 @@ export default {
     return {
       showNavbar: false,
       bookMenu: [
-        {index:1, name: 'part1', chapters: [{index:1, name: 'chapter1'}, {index:2, name: 'chapter2'}]},
-        {index:2, name: 'part2', chapters: [{index:3, name: 'chapter3'}, {index:4, name: 'chapter4'}]},
-        {index:3, name: 'part3', chapters: [{index:5, name: 'chapter5'}, {index:6, name: 'chapter6'}]}
+        {index:1, name: 'part1', chapters: [{index:11, name: 'chapter1'}, {index:12, name: 'chapter2'}]},
+        {index:2, name: 'part2', chapters: [{index:23, name: 'chapter3'}, {index:24, name: 'chapter4'}]},
+        {index:3, name: 'part3', chapters: [{index:35, name: 'chapter5'}, {index:36, name: 'chapter6'}]}
       ],
-      currentChapter: {partIndex: 1, chapterIndex: 1},
+      currentChapter: {partIndex: 1, chapterIndex: 12},
       currentPage: 1
     }
   },
--- vue/my_home_server/src/components/bookpage/BookDisplayNavbarMenu.vue
+++ vue/my_home_server/src/components/bookpage/BookDisplayNavbarMenu.vue
@@ -1,25 +1,28 @@
 <template>
 
   <b-list-group>
-    <b-list-group-item>
-      <div v-for="part in bookMenu" :key="part.index">
-        <b-button v-b-toggle="'book-display-navbar-index-' + part.index" class="m-1" variant="light">
-          {{ part.name }}
-        </b-button>
-        <b-collapse :id="'book-display-navbar-index-' + part.index">
-          <b-list-group>
-            <b-list-group-item
-              href="#"
-              :class="{active: currentChapter.chapterIndex == chapter.index}"
-              v-for="chapter in part.chapters"
-              :key="chapter.index"
-              @click="onChapterSelect(part.index, chapter.index)"
-              >
-              {{ chapter.name }}
-            </b-list-group-item>
-          </b-list-group>
-        </b-collapse>
-      </div>
+    <b-list-group-item v-for="part in bookMenu" :key="part.index">
+      <b-button
+        v-b-toggle="'book-display-navbar-index-' + part.index"
+        class="m-1"
+        variant="light"
+        :class="{active: currentChapter.partIndex == part.index}"
+        >
+        {{ part.name }}
+      </b-button>
+      <b-collapse :id="'book-display-navbar-index-' + part.index">
+        <b-list-group>
+          <b-list-group-item
+            button
+            v-for="chapter in part.chapters"
+            :class="{active: currentChapter.chapterIndex == chapter.index}"
+            :key="chapter.index"
+            @click="onChapterSelect(part.index, chapter.index)"
+            >
+            {{ chapter.name }}
+          </b-list-group-item>
+        </b-list-group>
+      </b-collapse>
     </b-list-group-item>
   </b-list-group>
 
